99f50704751fa2a93e9e56886dde0fcd7f225a69
chemiadel <pace3man@gmail.com>
2021-03-11 18:52:49 +0100

    a

--- pages/work.js
+++ pages/work.js
@@ -43,7 +43,7 @@ const works=[
     src:"https://github.com/chemiadel/nextjs-tailwindcss-portfolio",
     title: "Portfolio",
     link: "https://adeldev.vercel.app",
-    description: "My sample portfolio portfolio",
+    description: "My sample portfolio",
     badges: [
       <img alt="Next JS" src="https://img.shields.io/badge/next%20js%20-%23000000.svg?&style=for-the-badge&logo=next.js&logoColor=white"/>,
       <img alt="TailwindCSS" src="https://img.shields.io/badge/tailwindcss%20-%2338B2AC.svg?&style=for-the-badge&logo=tailwind-css&logoColor=white"/>
