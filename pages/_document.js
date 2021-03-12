f289f74857f5bf1e39d4eb869a979f54d3ce1d64
chemiadel <pace3man@gmail.com>
2021-03-12 21:14:41 +0100

    i

---
+++ pages/_document.js
@@ -0,0 +1,38 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+        <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-KHYMTW1B5D"
+          />
+
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-KHYMTW1B5D');
+                `,
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
